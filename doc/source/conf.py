b4e7290f345987f7891607bfb8a3dfaa0190f477
Sebastian Gode <sebastian.gode@telekom.de>
2023-08-29 07:25:04 +0000

    Fixed search filter

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,6 +27,7 @@ otcdocs_service_category = ''
 otcdocs_service_title = ''
 otcdocs_service_type = ''
 otcdocs_search_environment = 'hc_swiss'
+otcdocs_search_index = 'search_index_swiss'
 otcdocs_search_url = "https://opensearch.eco.tsi-dev.otc-service.com/"
 
 smartquotes = False
