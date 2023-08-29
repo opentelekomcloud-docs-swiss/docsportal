12da8c46cd23e55f90dc0fe230781516fc292976
SebastianGode <70581801+SebastianGode@users.noreply.github.com>
2023-08-29 10:01:21 +0200

    Fixed links (#8)
    Fixed links
    
    Reviewed-by: Tino Schr

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -19,7 +19,15 @@ extensions = [
 ]
 
 # Those variables are needed for indexing into OpenSearch
+otcdocs_doc_environment = ''
+otcdocs_doc_link = ''
+otcdocs_doc_title = ''
+otcdocs_doc_type = ''
+otcdocs_service_category = ''
+otcdocs_service_title = ''
+otcdocs_service_type = ''
 otcdocs_search_environment = 'hc_swiss'
+otcdocs_search_index = 'search_index_swiss'
 otcdocs_search_url = "https://opensearch.eco.tsi-dev.otc-service.com/"
 
 smartquotes = False
@@ -64,6 +72,7 @@ html_favicon = '_static/favicon.ico'
 html_theme_options = {
     'show_other_versions': False,
     'sidebar_mode': 'toctree',
+    "logo_url": "https://docs-beta.sc.otc.t-systems.com",
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
